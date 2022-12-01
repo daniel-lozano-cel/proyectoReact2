73790cfdc95d8325b9f4d9e7b49a205abfa19e4d
Daniel Lozano Cely <danielozano435@gmail.com>
2022-12-01 12:13:44 -0500

    added cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,28 @@
-const Cart = () => {
+import { useContext } from 'react';
+import { CartContext } from './CartContext';
+import '../App.css'
 
+const Cart = () =>{
+
+    const test = useContext(CartContext)
+    
     return(
         <>
+            <h1>
+                Cart
+            </h1>
+            {
+                test.length === 0
+                ? <span>The cart is empty</span>
+                : test.map(item=>
+                    <>
+                        <img src={item.pictureUrl}></img>
+                        <span>{item.title} - {item.qty} - {item.price}</span>
+                    </>
+                
+                )
+            }
         </>
     )
 }
-
 export default Cart;
